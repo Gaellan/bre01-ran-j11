b9f974f33e64a37d89408e004e337f2ccd271069
Gaellan <doucet.mari@gmail.com>
2023-10-30 13:57:23 +0000

    ex2 & ex3

---
+++ ex-shell/ex-2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cat marvel/iron_man.txt && echo '' && cat dc/batman.txt && echo '' 
+
+# ou
+
+cat marvel/iron_man.txt
+echo ''
+cat dc/batman.txt
+echo ''
---
+++ ex-shell/ex-3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd dc
+
+touch justice_league.txt
+
+cat batman.txt >> justice_league.txt
+echo '' >> justice_league.txt
+cat superman.txt >> justice_league.txt
+
+cd ..
